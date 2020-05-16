e473f8a2ff7d9a9342c42b92842a485a6be148cb
kvsrajawat <kvs.rajawat.bh@gmail.com>
2020-05-16 17:27:50 +0530

    optimized auto sync

--- contributor-service/src/main/java/com/ContibutorService/ContibutorContoller.java
+++ contributor-service/src/main/java/com/ContibutorService/ContibutorContoller.java
@@ -99,14 +99,14 @@ public class ContibutorContoller {
 //    			Sheets.Spreadsheets.Values.Update request = service.spreadsheets().values().update(spreadsheetId, updateRange, body);
 //    			request.setValueInputOption("USER_ENTERED");
 //    			request.execute();
-    			updateRange = "Beneficiary Database!Q"+(i+2);
+    			updateRange = "Beneficiary Database!Q"+(i+3);
     			values = Arrays.asList(
     	    	        Arrays.asList(
     	    	                "TRUE"
     	    	        )
     	    	);
-    			body = new ValueRange().setValues(values);
-    			request = service.spreadsheets().values().update(spreadsheetId, updateRange, body);
+    			ValueRange body = new ValueRange().setValues(values);
+    			Sheets.Spreadsheets.Values.Update request = service.spreadsheets().values().update(spreadsheetId, updateRange, body);
     			request.setValueInputOption("USER_ENTERED");
     			request.execute();
     			break;
